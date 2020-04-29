c26ee234c68fcfb35d0e90092e7d259865685bb1
ConWarOp <spitha3@hotmail.com>
2020-04-29 18:34:41 +0200

    Corridors
    Added end corridors

--- Assets/Resources/Scripts/Room_Generation/Utility/RoomFactory.cs
+++ Assets/Resources/Scripts/Room_Generation/Utility/RoomFactory.cs
@@ -23,8 +23,12 @@ public class RoomFactory
                 return new FightingRoom(position, tiles, type,tiles_x,tiles_z);
             case "Horizontal_Corridor":
                 return new Horizontal_Corridor(position, tiles, type, tiles_x, tiles_z);
+            case "Horizontal_End_Corridor":
+                return new Horizontal_End_Corridor(position, tiles, type, tiles_x, tiles_z);
             case "Vertical_Corridor":
                 return new Vertical_Corridor(position, tiles, type, tiles_x, tiles_z);
+            case "Vertical_End_Corridor":
+                return new Vertical_End_Corridor(position, tiles, type, tiles_x, tiles_z);
             default:
                 return new SpawningRoom(position, tiles,type);
         }
