fa26c085ae4e8b947786592a285e99f8c7f5cff2
Stathis Armaganidis <stathisarmaganidis@hotmail.com>
2020-12-01 10:53:34 +0200

    WisdomWell Design

---
+++ Assets/Resources/Scripts/WisdomWell/WellTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WellTrigger : MonoBehaviour
+{
+    public GameObject mark;
+    public GameObject peerButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        peerButton.SetActive(false);
+        mark.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            mark.SetActive(true);
+            peerButton.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            mark.SetActive(false);
+            peerButton.SetActive(false);
+        }
+    }
+
+    public void WellSpeak()
+    {
+
+    }
+}
---
+++ Assets/Resources/Scripts/WisdomWell/WisdomQuestions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WisdomQuestions
+{
+    Dictionary<string, (string, string, string)> extraDic = new Dictionary<string, (string, string, string)>
+    {
+        {"todo",("intro","neutral","extra") },
+        {"todo",("intro","neutral","extra") },
+        {"todo",("intro","neutral","extra") },
+        {"todo",("intro","neutral","extra") }
+    };
+    
+    Dictionary<string, (string, string, string)> neuroDic = new Dictionary<string, (string, string, string)>
+    {
+        {"todo",("stable","neutral","neuro") },
+        {"todo",("stable","neutral","neuro") },
+        {"todo",("stable","neutral","neuro") },
+        {"todo",("stable","neutral","neuro") }
+    };
+    
+    Dictionary<string, (string, string, string)> psychoDic = new Dictionary<string, (string, string, string)>
+    {
+        {"todo",("","neutral","neuro") },
+    };
+}
